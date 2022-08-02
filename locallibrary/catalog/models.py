d59067fd0b65150b1f917fca79552bd3222ef38d
Iryna Mikhieieva <medvedevaic@gmail.com>
2022-08-02 19:16:52 +0300

    Added language model Register models on the admin side Customize models view on the admin

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -10,6 +10,14 @@ class Genre(models.Model):
         return self.name
 
 
+class Language(models.Model):
+    name = models.CharField(max_length=200, help_text="Enter the book's natural language "
+                                                      "(e.g. English, French, Japanese etc.)")
+
+    def __str__(self):
+        return self.name
+
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
@@ -18,9 +26,19 @@ class Book(models.Model):
                                                                           '<a href="https://www.isbn-international.'
                                                                           'org/content/what-isbn">ISBN number</a>')
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        ordering = ['title', 'author']
 
     def __str__(self):
         return self.title
+
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
 
     def get_absolute_url(self):
         return reverse('book-detail', args=[str(self.id)])
